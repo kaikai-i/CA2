e378ed1d0441ee17d83f5ee5ca5ce69141a39321
kaikai-i <kailiz.25@ichat.sp.edu.sg>
2025-08-06 13:53:32 +0800

    Update CA2.ino

--- CA2.ino
+++ CA2.ino
@@ -6,14 +6,22 @@
 
 #include "RichShieldDHT.h"
 #include "RichShieldTM1637.h"
+#include "RichShieldPassiveBuzzer.h"
+
+#define PassiveBuzzerPin 3//the SIG pin of the module is connected with D3 of OPEN-SMART UNO R3
+                     //There is no white waterproof sticker on the passive buzzer
+PassiveBuzzer buz(PassiveBuzzerPin);
+
 #define CLK 10//CLK of the TM1637 IC connect to D10 of OPEN-SMART UNO R3
 #define DIO 11//DIO of the TM1637 IC connect to D11 of OPEN-SMART UNO R3
 #define LED_RED 4
 #define LED_GREEN 5
 #define LED_BLUE 6
 #define LED_YELLOW 7
+
+
 TM1637 disp(CLK,DIO);
-double H,T;
+double humi,t;
 
 DHT dht;
 
@@ -30,7 +38,8 @@ void loop() {
   float t = dht.readTemperature();
 
   // check if returns are valid, if they are NaN (not a number) then something went wrong!
-  if (isnan(t) || isnan(h)) {
+  if (isnan(t) || isnan(h)) 
+  {
     displayError();
   } 
   else{
@@ -81,34 +90,36 @@ void displayError()
 {
   disp.display(3,14);//display "E"
 }
-void setup()
+
+void setUp()
 {
   pinMode(LED_RED,OUTPUT);
   pinMode(LED_GREEN,OUTPUT);
   pinMode(LED_BLUE,OUTPUT);
   pinMode(LED_YELLOW,OUTPUT);
-}
-if (H<10||T<10)
+
+if (humi>40)
 {
-  cout<<"LOW HUMIDITY value\n";
+  Serial.println("LOW HUMIDITY value");
   void blink(int led,int msdelay);
 
+  digitalWrite(LED_RED, LOW);
   digitalWrite(LED_GREEN, LOW);
-  digitalWrite(LED_YELLOW, LOW);
-  digitalWrite(LED_RED,HIGH);
-  digitalWrite(LED_BLUE,LOW);
+  digitalWrite(LED_BLUE,HIGH);
+  digitalWrite(LED_YELLOW,LOW);
 	
-  for (int i = 0; i < ; i++)
-  {buz.playTone(392, 100);
-  delay(100);
+  for (int i = 0; i < 10; i++)
+  {  buz.playTone(392, 100);
+     delay(100);
   }
 }
 else
 { 
-  cout<<"HIGH HUMIDITY";	
-  digitalWrite(LED_GREEN,HIGH );
-  digitalWrite(LED_YELLOW,LOW );
-  digitalWrite(LED_RED,LOW );
-  digitalWrite(LED_BLUE,LOW);
+  Serial.println("HIGH HUMIDITY");	
+  digitalWrite(LED_RED,HIGH );
+  digitalWrite(LED_GREEN,LOW );
+  digitalWrite(LED_BLUE,LOW );
+  digitalWrite(LED_YELLOW,LOW);
   return 0;
+}
 }
